1a4bac8718f5f43123d98d0f2829817f0ea3110f
vermo <vermoorteligor@gmail.com>
2022-03-20 17:45:14 +0100

    init commit

---
+++ src/stockInformation/Stock.java
@@ -0,0 +1,111 @@
+package stockInformation;
+
+public class Stock {
+	private String tickerName;
+	private double priceEarnings;
+	private double lastPrice;
+	private double fiftyWeekLow;
+	private double fiftyWeekHigh;
+	private double marketCap;
+	private double earnings;
+	private double EPS;
+	private double trailingEPS;
+	private double trailingPE;
+
+
+
+	public Stock (String tickerName)  {
+
+		this.tickerName= tickerName;
+
+
+	}
+
+	public void calcPriceEarnings () {
+		priceEarnings = lastPrice/EPS;
+		trailingPE = lastPrice/trailingEPS;
+	}
+
+	public String getTickerName() {
+		return tickerName;
+	}
+
+
+
+	public void setTickerName(String tickerName) {
+		this.tickerName = tickerName;
+	}
+
+
+
+	public double getPriceEarnings() {
+		return priceEarnings;
+	}
+
+
+
+	public void setPriceEarnings(double priceEarnings) {
+		this.priceEarnings = priceEarnings;
+	}
+
+
+
+	public double getLastPrice() {
+		return lastPrice;
+	}
+
+
+
+	public void setLastPrice(double lastPrice) {
+		this.lastPrice = lastPrice;
+	}
+
+
+
+	public double getFiftyWeekLow() {
+		return fiftyWeekLow;
+	}
+
+
+
+	public void setFiftyWeekLow(double fiftyWeekLow) {
+		this.fiftyWeekLow = fiftyWeekLow;
+	}
+
+
+
+	public double getFiftyWeekHigh() {
+		return fiftyWeekHigh;
+	}
+
+
+
+	public void setFiftyWeekHigh(double fiftyWeekHigh) {
+		this.fiftyWeekHigh = fiftyWeekHigh;
+	}
+
+
+
+	public double getMarketCap() {
+		return marketCap;
+	}
+
+
+
+	public void setMarketCap(double marketCap) {
+		this.marketCap = marketCap;
+	}
+
+
+
+	public double getEarnings() {
+		return earnings;
+	}
+
+
+
+	public void setEarnings(double earnings) {
+		this.earnings = earnings;
+	}
+
+}
